eea13022a15c6da373da67d6219a90002f5cebdd
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-11-16 18:24:34 -0300

    Volta ao ItemCategories

--- MyApp/src/components/ItemCategories.jsx
+++ MyApp/src/components/ItemCategories.jsx
@@ -3,18 +3,6 @@ import { useEffect, useState } from 'react';
 function ApiCategories() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const ApiCategories = async () => {
-      const response = await fetch(
-        'https://api.mercadolibre.com/sites/MLB/categories'
-      );
-      const results = await response.json();
-      setData(results);
-      console.log(results);
-    };
-    ApiCategories();
-  }, [setData]);
-
   const arrayCategory = data.filter((index) => index);
 
   return (
--- MyApp/src/services/fetchItem.jsx
+++
@@ -1,5 +0,0 @@
-export async function fetchItem() {
-  const resposta = await fetch(`https://api.mercadolibre.com/items/Patins}`);
-  const response = await resposta.json();
-  return response;
-}
